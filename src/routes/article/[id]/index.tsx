a41ba079055789a04112781afc8486826a79ae53
Nicolas Choquet <nchoquet@norsys.fr>
2023-06-16 11:06:06 +0200

    Ajout d'une page 404 ( non fonctionnelle pour le moment ) Ajout d'une page à url dynamique ( si le header "accept" est défnit à "application/json", le contenu de la page est renvoyé au format json au lieux du format html )

--- src/routes/article/[id]/index.tsx
+++ src/routes/article/[id]/index.tsx
@@ -1,10 +1,36 @@
 import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { RequestHandler, useLocation } from '@builder.io/qwik-city';
+
+export const onGet: RequestHandler = async ({ params, request, headers, url, redirect, json }) => {
+    if (params.id == 12) {
+        const redirectUrl = new URL('/', url).toString();
+
+        if (
+            headers.get('accept') === 'application/json' ||
+            request.headers.get('accept') === 'application/json'
+        ) {
+            throw json(308, {
+                code: 308,
+                redirect: redirectUrl
+            });
+        }
+
+        throw redirect(308, redirectUrl);
+    } else if (
+        headers.get('accept') === 'application/json' ||
+        request.headers.get('accept') === 'application/json'
+    ) {
+        throw json(200, {
+            code: 200,
+            title: `coucou ${params.id}`
+        })
+    }
+}
 
 export default component$(() => {
-    const route = useLocation();
+    const { params } = useLocation();
     
     return (<>
-        <h1>coucou {route.params.id}</h1>
+        <h1>coucou {params.id}</h1>
     </>)
 });
